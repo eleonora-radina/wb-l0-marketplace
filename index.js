22a7a8eb202a5c974e386bbf30d9dc7d05196d74
Eleonora Nikerina <eleonora.radina@mail.ru>
2023-11-10 19:05:23 +0400

    add popup opening

--- index.js
+++ index.js
@@ -8,6 +8,17 @@ const accordionMissingButton = document.querySelector('.basket__list-icon_missin
 const accordionMissingList = document.querySelector('.basket__list-zone_missing');
 const basketMissingList = document.querySelector('.basket__missing-list');
 
+const buttonEditDelivery = document.querySelector('.basket__form-button_delivery');
+const buttonEditPayment = document.querySelector('.basket__form-button_payment');
+const buttonSidebarEditDelivery = document.querySelector('.sidebar__info-edit-button_delivery');
+const buttonSidebarPaymentDelivery = document.querySelector('.sidebar__info-edit-button_payment');
+
+const popupDeliveryForm = document.querySelector('.popup_delivery');
+const popupPaymentForm = document.querySelector('.popup_payment');
+const buttonCloseDeliveryPopup = document.querySelector('.popup_delivery .popup__button-exit');
+const buttonClosePaymentPopup = document.querySelector('.popup_payment .popup__button-exit');
+
+
 const toggleAccordionList = () => {
   accordionList.classList.toggle('hidden');
   accordionButton.classList.toggle('rotated');
@@ -16,12 +27,27 @@ const toggleAccordionList = () => {
   basketListHeader.classList.toggle('line');
 }
 
-accordionButton.addEventListener('click', () => toggleAccordionList());
-
 const toggleAccordionMissingList = () => {
   accordionMissingList.classList.toggle('hidden');
   accordionMissingButton.classList.toggle('rotated');
   basketMissingList.classList.toggle('hidden-missing-list');
 }
 
+const openPopup = (popup) => {
+  popup.classList.add('popup_opened');
+}
+
+const closePopup = (popup) => {
+  popup.classList.remove('popup_opened');
+}
+
+accordionButton.addEventListener('click', () => toggleAccordionList());
 accordionMissingButton.addEventListener('click', () => toggleAccordionMissingList());
+
+buttonEditDelivery.addEventListener('click', () => openPopup(popupDeliveryForm));
+buttonEditPayment.addEventListener('click', () => openPopup(popupPaymentForm));
+buttonSidebarEditDelivery.addEventListener('click', () => openPopup(popupDeliveryForm));
+buttonSidebarPaymentDelivery.addEventListener('click', () => openPopup(popupPaymentForm));
+
+buttonCloseDeliveryPopup.addEventListener('click', () => closePopup(popupDeliveryForm));
+buttonClosePaymentPopup.addEventListener('click', () => closePopup(popupPaymentForm));
