3458dff0306b232df9a9216588bff2b7445ce883
Eleonora Nikerina <eleonora.radina@mail.ru>
2023-11-11 01:17:24 +0400

    add form validation

--- js/popups.js
+++ js/popups.js
@@ -1,13 +1,3 @@
-const accordionButton = document.querySelector('.basket__list-icon');
-const accordionList = document.querySelector('.basket__list-zone');
-const accordionCheckbox = document.querySelector('.form__label');
-const accordionItems = document.querySelector('.basket__list-header-items');
-const basketListHeader = document.querySelector('.basket__list-header');
-
-const accordionMissingButton = document.querySelector('.basket__list-icon_missing');
-const accordionMissingList = document.querySelector('.basket__list-zone_missing');
-const basketMissingList = document.querySelector('.basket__missing-list');
-
 const buttonEditDelivery = document.querySelector('.basket__form-button_delivery');
 const buttonEditPayment = document.querySelector('.basket__form-button_payment');
 const buttonSidebarEditDelivery = document.querySelector('.sidebar__info-edit-button_delivery');
@@ -17,21 +7,6 @@ const popupDeliveryForm = document.querySelector('.popup_delivery');
 const popupPaymentForm = document.querySelector('.popup_payment');
 const buttonCloseDeliveryPopup = document.querySelector('.popup_delivery .popup__button-exit');
 const buttonClosePaymentPopup = document.querySelector('.popup_payment .popup__button-exit');
-
-
-const toggleAccordionList = () => {
-  accordionList.classList.toggle('hidden');
-  accordionButton.classList.toggle('rotated');
-  accordionCheckbox.classList.toggle('hidden');
-  accordionItems.classList.toggle('hidden');
-  basketListHeader.classList.toggle('line');
-}
-
-const toggleAccordionMissingList = () => {
-  accordionMissingList.classList.toggle('hidden');
-  accordionMissingButton.classList.toggle('rotated');
-  basketMissingList.classList.toggle('hidden-missing-list');
-}
 
 const openPopup = (popup) => {
   popup.classList.add('popup_opened');
